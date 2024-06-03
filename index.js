fddfdea0e50e00444c41ffe2e3ac22821b071221
remo251128 <remo251128@gmail.com>
2024-06-03 00:25:22 -0500

    cambio

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Reload button functionality
     const reloadButton = document.querySelector('.reload-button');
-    reloadButton.addEventListener('click', function () {
-        location.reload();
-    });
+    reloadButton.addEventListener('click', () => location.reload());
 
     const imageContainer = document.getElementById('imageContainer');
     let startRow = 0;
     const rowsPerLoad = 2;
-    const maxArticles = 100; // Maximum number of articles to display
+    const maxArticles = 100;
     let loading = false;
     let displayedArticles = {};
 
-    function loadMoreArticles() {
+    const loadMoreArticles = () => {
         loading = true;
         const articlesRemaining = maxArticles - Object.keys(displayedArticles).length;
         const articlesToLoad = Math.min(rowsPerLoad * 3, articlesRemaining);
+
         fetch(`noticias.json?start=${startRow * 3}&limit=${articlesToLoad}`)
             .then(response => response.json())
             .then(data => {
                 if (data && data.images && data.images.length > 0) {
-                    displayImages(data.images.reverse()); // Reverse the array
+                    displayImages(data.images.reverse());
                     startRow += rowsPerLoad;
                     loading = false;
                 } else {
@@ -31,121 +29,101 @@ document.addEventListener('DOMContentLoaded', function () {
                 console.error('Error fetching articles:', error);
                 loading = false;
             });
-    }
+    };
 
-    function displayImages(imageFiles) {
+    const displayImages = (imageFiles) => {
         imageFiles.forEach((filename, index) => {
-            // Check if article has been displayed before and limit the number of articles
             if (!displayedArticles[filename] && Object.keys(displayedArticles).length < maxArticles) {
                 const block = document.createElement('div');
                 block.classList.add('block');
 
-                const imgLink = document.createElement('a'); // Create anchor element for the image
-                imgLink.href = `article.html?image=${filename}`; // Link to article page
+                const imgLink = document.createElement('a');
+                imgLink.href = `article.html?image=${filename}`;
 
                 const imgElement = new Image();
                 imgElement.src = `Imagenes/${filename}`;
                 imgElement.alt = `Image ${startRow * 3 + index + 1}`;
                 imgElement.classList.add('resizable-image');
 
-                const title = document.createElement('a'); // Create title element
+                const title = document.createElement('a');
                 title.classList.add('image-title');
                 title.style.fontFamily = 'sans-serif';
-                title.style.fontSize = '20px'; // Increased font size
-                title.style.fontWeight = 'bold'; // Bold text
-                title.style.color = '#000000'; // Completely black color
-                title.style.textDecoration = 'none'; // No underline
-                title.style.marginTop = '20px'; // Reduced vertical margin between text and image
-                title.style.marginBottom = '30px'; // Increased vertical margin between rows
-                title.style.display = 'block'; // Centered text
+                title.style.fontSize = '20px';
+                title.style.fontWeight = 'bold';
+                title.style.color = '#000000';
+                title.style.textDecoration = 'none';
+                title.style.marginTop = '20px';
+                title.style.marginBottom = '30px';
+                title.style.display = 'block';
                 title.style.textAlign = 'center';
 
-                // Fetch the content of the corresponding .txt file
                 fetch(`Texto/${filename.replace(/\.[^/.]+$/, "")}.txt`)
                     .then(response => response.text())
                     .then(text => {
-                        title.textContent = text.split('\n')[0]; // Use the first line of the text file as the title
-                        title.href = `article.html?image=${filename}`; // Link to article page
+                        title.textContent = text.split('\n')[0];
+                        title.href = `article.html?image=${filename}`;
                     })
-                    .catch(error => {
-                        console.error('Error fetching text file:', error);
-                    });
+                    .catch(error => console.error('Error fetching text file:', error));
 
-                imgLink.appendChild(imgElement); // Append image to anchor element
-                block.appendChild(imgLink); // Append anchor element to block
-                block.appendChild(title); // Append title below image
-
+                imgLink.appendChild(imgElement);
+                block.appendChild(imgLink);
+                block.appendChild(title);
                 imageContainer.appendChild(block);
 
-                // Mark article as displayed
                 displayedArticles[filename] = true;
             }
         });
 
-        // After all images are added, adjust their sizes
         adjustImageSizes();
-    }
+    };
 
-    function adjustImageSizes() {
+    const adjustImageSizes = () => {
         const images = document.querySelectorAll('.resizable-image');
         const containerWidth = imageContainer.offsetWidth;
-        const margin = 20; // Margin between images
+        const margin = 20;
         const numImagesPerRow = 3;
         const totalMarginWidth = (numImagesPerRow - 1) * margin;
         const imageWidth = (containerWidth - totalMarginWidth) / numImagesPerRow;
-        const imageHeight = imageWidth * 9 / 16; // Assuming images have a 16:9 aspect ratio
+        const imageHeight = imageWidth * 9 / 16;
 
-        // Set fixed width and height for all images in desktop view
         images.forEach(img => {
             if (window.innerWidth <= 600) {
-                img.style.width = 'calc(100% - 10px)'; // Almost full width for mobile
-                img.style.height = 'auto'; // Maintain aspect ratio
-                img.style.margin = '0 5px'; // Margin for mobile view
+                img.style.width = 'calc(100% - 10px)';
+                img.style.height = 'auto';
+                img.style.margin = '0 5px';
             } else {
                 img.style.width = imageWidth + 'px';
                 img.style.height = imageHeight + 'px';
             }
         });
-    }
+    };
 
-    function changeBannerImage() {
-        const banner = document.querySelector('.banner'); // Select the banner element
-        const bannerImages = ['Banner/1.png', 'Banner/2.png', 'Banner/3.png', 'Banner/4.png', 'Banner/5.png']; // Array of banner images
-        let currentImageIndex = 0; // Variable to keep track of current banner image index
+    const changeBannerImage = () => {
+        const banner = document.querySelector('.banner');
+        const bannerImages = ['Banner/1.png', 'Banner/2.png', 'Banner/3.png', 'Banner/4.png', 'Banner/5.png'];
+        let currentImageIndex = 0;
 
-        // Set the first image immediately
-        banner.style.backgroundSize = 'cover'; // Stretch image to fit fixed width and height
+        banner.style.backgroundSize = 'cover';
         banner.style.backgroundRepeat = 'no-repeat';
         banner.style.backgroundImage = `url(${bannerImages[currentImageIndex]})`;
 
         setInterval(() => {
-            // Preload the next image for smooth transition
             const nextImage = new Image();
             nextImage.src = bannerImages[(currentImageIndex + 1) % bannerImages.length];
-
-            // Smooth transition between images
             banner.style.transition = 'background-image 1s ease-in-out';
-
-            // Set the background image of the banner
             banner.style.backgroundImage = `url(${nextImage.src})`;
-
-            // Increment index and loop back to 0 if it exceeds the array length
             currentImageIndex = (currentImageIndex + 1) % bannerImages.length;
-        }, 5000); // Change image every 5 seconds
-    }
+        }, 5000);
+    };
 
-    // Load the first rowsPerLoad initially
     loadMoreArticles();
 
-    // Load additional rows as the user scrolls down
-    window.addEventListener('scroll', function () {
+    window.addEventListener('scroll', () => {
         if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight && !loading) {
             loadMoreArticles();
         }
     });
 
     changeBannerImage();
-
-    // Adjust image sizes on window resize
     window.addEventListener('resize', adjustImageSizes);
 });
